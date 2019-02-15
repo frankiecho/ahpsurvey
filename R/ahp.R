18d251b52959d7867f40f4fb437990ee4d3a4b82
frankiecho <htcho@connect.hku.hk>
2019-02-16 02:40:27 +0800

    Added descriptions for new functions

--- R/ahp.R
+++ R/ahp.R
@@ -15,6 +15,9 @@
 #' @param qt specifies the quantile which the top **and** bottom priority weights are trimmed. Used only if `aggmethod = 'tmean'` or `aggmethod = 'tgmean'`. For example, `qt = 0.25` specifies that the aggregation is the arithmetic mean of the values from the 25 to 75 percentile. By default `qt = 0`.
 #' @param censorcr the threshold of censoring observations. All observations with a consistency ratio higher than the specified CR will be dropped from the routine. By default, ``censorcr = Inf``, i.e. drops no observations.
 #' @param agg by default, `agg = FALSE`, which suppresses the output of Aggregated priorities. If `agg = TRUE`, the output will be a list where `$AggPref` contains the Aggregated priorities and Standard Deviations.
+#' @param ID the column name, or a vector of column names, of variables in `df`, which are used to indicate specific observations. For example, if a survey dataset comes with an identifier/ multiple identified for each observation, use this so that the output dataframe would contain this identifier.
+#' @param col a list of column names which contains the columns for the pairwise comparison matrix in chronological order, as specified in `atts`. For example, if `atts` is `c("A","B","C")`, then `col` would be `c("A_B","A_C","B_C")`, with `"A_B"` being a pairwise comparison of attribute A and B. `col = NULL` by default, indicating that the dataframe specified already contained the pairwise comparisons in correct order and nothing else except of what is inside `ID`.
+#' @param suppress Suppresses the supplementary indices. Can take the values of "all" (suppresses all indices), "pwerror" (suppresses pwerror), or "cr" (suppresses consistency ratio).
 #' @return If ``agg = TRUE``, returns a list of the results with two elements, `indpref` and `aggpref`. `$indpref` contains individual preference weight (same as `ahp.indpref`), consistency ratios, and the top three most inconsistent pairwise comparisons (same as `ahp.pwerror`). `$aggpref` contains the Aggregated Priorities and Standard Deviations. Otherwise (i.e. default setting), outputs a `data.frame` with only `$indpref`.
 #'
 #' @examples
@@ -32,31 +35,111 @@
 #'@export
 #'
 
-ahp <- function(df, atts, negconvert = FALSE, reciprocal = TRUE, method = 'eigen', aggmethod = 'geometric', qt = 0, censorcr = Inf, agg = FALSE){
-  ahpmat <- ahp.mat(df = df, atts = atts, negconvert = negconvert, reciprocal = reciprocal)
+ahp <- function (df, atts, negconvert = FALSE, reciprocal = TRUE, method = "eigen", 
+                 aggmethod = "geometric", qt = 0, censorcr = Inf, agg = FALSE, ID = NULL, 
+                 col = NULL, suppress = "none"){
+  # Save names in a separate column
+  if (is.null(ID)==FALSE) {
+    if (is.vector(ID) == FALSE & is.character(ID) == FALSE) {
+      stop("ID must be a vector or character.")
+    }
+    
+    # Check if ID is in the data frame
+    for (n in ID){
+      if (n %in% names(df) == FALSE){
+        stop("ID is not in dataframe supplied. Mistyped ID column name?")
+      }
+    }
+    
+    ID_saved <- df[,ID]
+    
+    # Remove data frame ID column
+    df[,ID] <- NULL
+  }
+  
+  # If columns are selected, then order it based on a particular order
+  if (is.null(col) == FALSE){
+    
+    # Check if the column names were correctly specified
+    for (n in col){
+      if (n %in% names(df) == FALSE){
+        stop("One or more elements in col are not in dataframe supplied. Mistyped col column name?")
+      }
+    }
+    
+    # Subset dataframe based on col
+    df <- df[,col]
+  }
+  
+  # Stop if Censor CR = 0, because that is simply wrong
+  if (censorcr <= 0) stop("Censor CR cannot be equal or smaller than zero, because you will be throwing away all of your observations.")
+  
+  # Check whether everything in the data frame is within -9 and 9
+  for (i in 1:nrow(df)){
+    for (j in 1:ncol(df)){
+      if (is.numeric(df[i,j]) == FALSE) {
+        stop("Non numeric elements in data frame supplied! Is there an ID column in there?")
+      } else if (df[i,j] > 9 | df[i,j] < -9){
+        stop("There is something larger than 9 or smaller than -9 in your dataset. Miscoded?")
+      }
+    }
+  }
+  
+  # Generate a AHP matrix
+  ahpmat <- ahp.mat(df = df, atts = atts, negconvert = negconvert, 
+                    reciprocal = reciprocal)
+  
+  # Calculate consistency ratios
   cr <- ahp.cr(ahpmat = ahpmat, atts = atts)
   
-  ## Censor observations based on CR -- generate list of conditions of whether to keep or not
+  # Dummy code CR into remove (0) or not (1)
   censorlist <- sapply(cr, function(x) x <= censorcr)
   
-  ## make a new censored AHP mat
+  # Keep only the consistent ahp matrices
   cahpmat <- ahpmat[censorlist]
   
-  ## Print number of censored observations
-  print(paste("Number of observations censored =", length(censorlist[censorlist==FALSE])))
+  # Make a list of only the non-censored people ID
   
-  ## Recalculate list of CR based on censored AHP mat
-  ccr <- ahp.cr(ahpmat = cahpmat, atts = atts)
+    if (is.null(ID)==FALSE) {
+      if (ncol(ID_saved) > 1){
+        cID <- ID_saved[censorlist,]
+      } else {
+    cID <- ID_saved[censorlist]
+      }
+    }
   
+  
+  # Print number of censored observations
+  if (censorcr != Inf) print(paste("Number of observations censored =", length(censorlist[censorlist == FALSE])))
+  
+  # Make individual prirorities
   indpref <- ahp.indpref(ahpmat = cahpmat, atts = atts, method = method)
+  
+  # Make pairwise comparison errors
   pwerror <- ahp.pwerror(ahpmat = cahpmat, atts = atts)
   
-  output <- cbind(indpref, CR = ccr, pwerror, row.names = NULL)
+  if (is.null(ID)==FALSE) {
+    output <- cbind(cID, indpref, CR = cr[censorlist], pwerror, row.names = NULL)
+  } else {
+    output <- cbind(indpref, CR = cr[censorlist], pwerror, row.names = NULL)
+  }
   
-  if (agg == TRUE){
-    agg <- ahp.aggpref(ahpmat = cahpmat, atts = atts, method = method, aggmethod = aggmethod, qt = qt)
-    agg.sd <- ahp.aggpref(ahpmat = cahpmat, atts = atts, method = method, aggmethod = 'sd', qt = qt)
-    agg.output <- cbind(AggPref = agg, SD.AggPref = agg.sd, row.names = NULL)
+  if (suppress == "all"){
+    output$CR <- NULL
+    output$top1 <- output$top2 <- output$top3 <- NULL
+  } else if (suppress == "cr"){
+    output$CR <- NULL
+  } else if (suppress == "pwerror"){
+    output$top1 <- output$top2 <- output$top3 <- NULL
+  }
+  
+  if (agg == TRUE) {
+    agg <- ahp.aggpref(ahpmat = cahpmat, atts = atts, method = method, 
+                       aggmethod = aggmethod, qt = qt)
+    agg.sd <- ahp.aggpref(ahpmat = cahpmat, atts = atts, 
+                          method = method, aggmethod = "sd", qt = qt)
+    agg.output <- cbind(AggPref = agg, SD.AggPref = agg.sd, 
+                        row.names = NULL)
     output <- list(indpref = output, aggpref = agg.output)
   }
   return(output)
